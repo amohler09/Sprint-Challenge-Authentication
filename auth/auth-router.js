810b11c05312b99b47240a954cd60fcb419d69e7
Amber Mohler <ambermohler@Ambers-iMac.local>
2020-03-27 11:29:27 -0400

    added users-model.js and created auth functions

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const bcrpyt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 router.post('/register', (req, res) => {
-  // implement registration
+  // implement register
 });
 
 router.post('/login', (req, res) => {
